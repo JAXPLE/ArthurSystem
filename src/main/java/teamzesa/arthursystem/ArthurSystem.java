d39f09edc7db2f868df39bdbb8c3b65c1327903a
JwonLee <jaxple@gmail.com>
2023-05-27 14:59:29 +0900

    Update remove Code & Add new Event

--- src/main/java/teamzesa/arthursystem/ArthurSystem.java
+++ src/main/java/teamzesa/arthursystem/ArthurSystem.java
@@ -3,9 +3,10 @@ package teamzesa.arthursystem;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import teamzesa.command.StreamTotemStacking;
 import teamzesa.command.TotemStacking;
+import teamzesa.manager.NetherManager;
 import teamzesa.manager.PvpManager;
+import teamzesa.manager.RaidManager;
 
 
 public final class ArthurSystem extends JavaPlugin implements Listener {
@@ -13,6 +14,8 @@ public final class ArthurSystem extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new PvpManager(),this);
+        this.getServer().getPluginManager().registerEvents(new RaidManager(),this);
+        this.getServer().getPluginManager().registerEvents(new NetherManager(),this);
 
         Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
 
