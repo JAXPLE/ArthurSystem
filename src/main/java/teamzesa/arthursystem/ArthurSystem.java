c704809fc424c1835a2dc1f482da001910e0b92b
JwonLee <jaxple@gmail.com>
2023-05-31 18:52:11 +0900

    Add UserManager & remove ItemManager

--- src/main/java/teamzesa/arthursystem/ArthurSystem.java
+++ src/main/java/teamzesa/arthursystem/ArthurSystem.java
@@ -5,10 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.command.TotemStacking;
-import teamzesa.manager.AttackSpeedManager;
-import teamzesa.manager.NetherManager;
-import teamzesa.manager.PvpManager;
-import teamzesa.manager.RaidManager;
+import teamzesa.manager.*;
 
 
 public final class ArthurSystem extends JavaPlugin implements Listener {
@@ -22,12 +19,12 @@ public final class ArthurSystem extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.pm.registerEvents(AttackSpeedManager.getInstance(),this);
+        this.pm.registerEvents(UserManager.getInstance(),this);
         this.pm.registerEvents(new PvpManager(),this);
         this.pm.registerEvents(new RaidManager(),this);
         this.pm.registerEvents(new NetherManager(),this);
 
         Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
-
-//        ItemManager.customItem(this);
     }
+
 }
