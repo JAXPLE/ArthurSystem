a420af6d4ba33888d353b0dc0756b0f02079a4ae
JwonLee <jaxple@gmail.com>
2023-06-12 10:22:22 +0900

    Refactor

--- src/main/java/teamzesa/arthursystem/ArthurSystem.java
+++ src/main/java/teamzesa/arthursystem/ArthurSystem.java
@@ -1,7 +1,10 @@
 package teamzesa.arthursystem;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.command.TotemStacking;
@@ -11,20 +14,24 @@ import teamzesa.manager.*;
 public final class ArthurSystem extends JavaPlugin implements Listener {
 
     private PluginManager pm;
+    private AttackSpeedManager attackSpeedManager;
+    private HealthManager healthManager;
 
     public ArthurSystem() {
         this.pm = getServer().getPluginManager();
+        this.attackSpeedManager = AttackSpeedManager.getInstance();
+        this.healthManager = HealthManager.getInstance();
     }
 
     @Override
     public void onEnable() {
-        this.pm.registerEvents(AttackSpeedManager.getInstance(), this);
-//        this.pm.registerEvents(UserManager.getInstance(), this);
-//        this.pm.registerEvents(new PvpManager(), this);
+        this.pm.registerEvents(attackSpeedManager, this);
+        this.pm.registerEvents(healthManager, this);
         this.pm.registerEvents(new RaidManager(), this);
         this.pm.registerEvents(new NetherManager(), this);
+//        this.pm.registerEvents(UserManager.getInstance(), this);
+//        this.pm.registerEvents(new PvpManager(), this);
 
         Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
     }
-
 }
