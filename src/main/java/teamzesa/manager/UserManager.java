ece4a21d56155b27a19239e693d702033dc22076
JwonLee <jaxple@gmail.com>
2023-06-13 11:31:15 +0900

    Add saveUserInfo method

--- src/main/java/teamzesa/manager/UserManager.java
+++ src/main/java/teamzesa/manager/UserManager.java
@@ -1,6 +1,7 @@
 package teamzesa.manager;
 
-import com.google.gson.Gson;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,21 +10,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.arthursystem.ArthurSystem;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.UTFDataFormatException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
 public class UserManager implements Listener {
-    private static UserManager instance = new UserManager();
-    private static final String FILE = "playerHealthData.yml";
+    private static UserManager instance;
     private Map<UUID, Double> userInfo;
-    private File file;
+    private FileConfiguration config;
+    private File configFile;
+
+//    private UserManager() {
+////        userInfo = new HashMap<>();
+//    }
 
     private UserManager() {
         userInfo = new HashMap<>();
+        config = new YamlConfiguration();
     }
 
     public static UserManager getInstance() {
@@ -34,6 +38,26 @@ public class UserManager implements Listener {
 
     public void updatePlayerHealth(UUID playerUUID, double healthScale) {
         userInfo.replace(playerUUID, healthScale);
+    }
+
+    public void saveUserHealthInfo() {
+        for (UUID playerUUID : userInfo.keySet()) {
+            config.set("UUID:" + playerUUID.toString(), 0);
+            config.set("HealthScale:" + String.valueOf(userInfo.get(playerUUID)), 0);
+        }
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setConfig(FileConfiguration config) {
+        this.config = config;
+    }
+
+    public void setConfigFile(File configFile) {
+        this.configFile = configFile;
     }
 
     @EventHandler
@@ -47,10 +71,7 @@ public class UserManager implements Listener {
         p.setHealthScale(userInfo.get(p.getUniqueId()));
     }
 
-    public void savePlayerData() throws Exception{
-        Gson gson = new Gson();
-        String data = gson.toJson(userInfo);
-        file = new File(data);
-//        FileWriter fileWriter = new FileWriter(file,);
+    public Map<UUID,Double> getUserInfo() {
+        return userInfo;
     }
 }
